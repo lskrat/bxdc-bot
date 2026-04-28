551d6657dd5d7ad2934b6c7ebbc28cc0c1f4e37f
lskrat <lskrat@sina.com>
2026-04-28 13:37:20 +0800

    fix(gateway): switch to Apache HttpClient5 to support restricted headers like Origin

--- backend/skill-gateway/src/main/java/com/lobsterai/skillgateway/config/SkillGatewayHttpClientConfig.java
+++ backend/skill-gateway/src/main/java/com/lobsterai/skillgateway/config/SkillGatewayHttpClientConfig.java
@@ -1,11 +1,13 @@
 package com.lobsterai.skillgateway.config;
 
 import com.lobsterai.skillgateway.audit.GatewayHttpClientAuditInterceptor;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.BufferingClientHttpRequestFactory;
 import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Collections;
@@ -15,8 +17,14 @@ public class SkillGatewayHttpClientConfig {
 
     @Bean
     public RestTemplate gatewayRestTemplate(GatewayHttpClientAuditInterceptor auditInterceptor) {
-        ClientHttpRequestFactory underlying = new SimpleClientHttpRequestFactory();
-        BufferingClientHttpRequestFactory buffering = new BufferingClientHttpRequestFactory(underlying);
+        var connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setMaxTotal(50);
+        connectionManager.setDefaultMaxPerRoute(20);
+        var httpClient = HttpClients.custom()
+                .setConnectionManager(connectionManager)
+                .build();
+        ClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
+        BufferingClientHttpRequestFactory buffering = new BufferingClientHttpRequestFactory(factory);
         RestTemplate restTemplate = new RestTemplate(buffering);
         restTemplate.setInterceptors(Collections.singletonList(auditInterceptor));
         return restTemplate;
